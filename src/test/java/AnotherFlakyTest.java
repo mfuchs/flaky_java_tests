88efe77e9b0a1580e6c415eb994dd7232d62b448
Matthias Fuchs <matthias.s.fuchs@gmail.com>
2016-12-11 18:55:33 +0100

    Some flaky tests.

---
+++ src/test/java/AnotherFlakyTest.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AnotherFlakyTest {
+    @Test
+    public void testFlaky1() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+}
---
+++ src/test/java/FlakyTest.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class FlakyTest {
+    @Test
+    public void testFlaky1() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 2);
+    }
+
+    @Test
+    public void testFlaky2() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 4);
+    }
+}
